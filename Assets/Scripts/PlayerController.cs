a18a92b63937c2528b01ba3cf9e86d615462f930
ariadnetaaa04 <ariadna.perez.0401@gmail.com>
2022-11-15 13:01:20 +0100

    15/11/22

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20f;
-    public float turnSpeed = 10f;
+    //velocidad por fotograma
+    private float speed = 20f;
+    //
+    private float turnSpeed = 10f;
+    //variabe to move right or left
     private float horizontalInput;
+    //variable to move frontwards or backwards
     private float verticalInput;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +23,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // transform.Translate(0, 0, 1);
+        //Detect the inputs
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+        //Manual forward movement
         transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
+        //Manual lateral movement
+        /*
         transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput);
+        */
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+        
     }
 }
